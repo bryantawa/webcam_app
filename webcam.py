56ef21ce6938b75e89f6e8937356905a1a84ce7d
bryan <bryantawa@gmail.com>
2023-08-06 08:02:05 -0400

    Initial Commit

---
+++ webcam.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+with st.expander("Start Camera"):
+    # start the camera
+    camera_image = st.camera_input("Camera")
+
+if camera_image:
+    # create a Pillow image instance
+    img = Image.open(camera_image)
+
+    # Convert Pillow image to grayscale
+    gray_img = img.convert("L")
+
+    # Render the grayscale image on the browser
+    st.image(gray_img)
